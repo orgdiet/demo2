af482e1f2b778b4335465f7f66e02607c3b3944c
Tony Dietrich <tony.dietrich@capgemini.com>
2019-11-24 12:27:24 +0000

    Commit of cloned files

---
+++ src/test/java/com/yara/it/apieconomy/ms/demo1/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yara.it.apieconomy.ms.demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
